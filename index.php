409a31b0aecb40bb02261ff19b22f0d6030c01cd
Pedro <plbertonm@gmail.com>
2026-09-15 11:31:58 -0300

    Adiciona tecnologias utilizadas

--- index.php
+++ index.php
@@ -7,17 +7,27 @@
 <body>
     <h1>Projeto da dupla</h1>
     <p>Projeto criado para praticar Git e GitHub.</p>
+
     <h2>Membros do projeto</h2>
     <ul>
         <li>Pedro Hofman Barcelos: B</li>
         <li>Pedro Lucidonio Berton Marques: A</li>
         <li>Nathanael Luca Serafin: A</li>
     </ul>
+
     <h2>Sobre o projeto</h2>
     <p>
      Este projeto demonstra o desenvolvimento colaborativo
      utilizando Git e GitHub.
     </p>
+
+    <h2>Tecnologias utilizadas</h2>
+    <ul>
+        <li>PHP</li>
+        <li>HTML</li>
+        <li>Git</li>
+        <li>GitHub</li>
+    </ul>
 
 </body>
 </html>
