899df4bcbac95de200e7dec7b1e146060923e79d
Pedro <plbertonm@gmail.com>
2026-09-15 11:06:09 -0300

    Cria página inicial do projeto

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Projeto Git</title>
+</head>
+<body>
+    <h1>Projeto da dupla</h1>
+    <p>Projeto criado para praticar Git e GitHub.</p>
+    <h2>Membros do projeto</h2>
+    <ul>
+        <li>Pedro H Barcelos: B</li>
+        <li>Pedro L B Marques: A</li>
+        <li>Nathanael Luca Serafin: A</li>
+    </ul>
+</body>
+</html>
